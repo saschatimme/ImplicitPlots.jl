8ad43e2c4090aadf143251a295661f668ffb28b8
Sascha Timme <sascha.timme@gmail.com>
2019-10-08 12:05:04 +0200

    Use Plots.jl for curves in the plane + makie optional

--- README.md
+++ README.md
@@ -8,18 +8,19 @@ Plots curves and surfaces defined by a polynomial `f(x,y)=0` resp. `f(x,y,z)=0`.
 ```julia
 using ImplicitPlots
 using DynamicPolynomials # input
-using Makie # GLMakie backend
 
 @polyvar x y
 f =  (x^4 + y^4 - 1) * (x^2 + y^2 - 2) + x^5 * y
-implicit_plot(f; x_min=-3., x_max=3., y_min=-4, y_max=4)
+implicit_plot(f; xlims=(-3,3) ymax=(-3,3))
 ```
 
 ## Surface
+
+For surfaces need to have `AbstractPlotting` installed.
 ```julia
 using ImplicitPlots
 using DynamicPolynomials # input
-using Makie # GLMakie backend
+using Makie # GLMakie backend for AbstractPlotting
 
 @polyvar x y z
 g = (0.3*x^2+0.5z-0.3x+1.2*y^2-1.1)^2+(0.7*(y+0.5x)^2+y+1.2*z^2-1)^2-0.3
--- src/ImplicitPlots.jl
+++ src/ImplicitPlots.jl
@@ -2,97 +2,106 @@ module ImplicitPlots
 
 export implicit_plot
 
-import AbstractPlotting
 import ColorSchemes, Contour
 import GeometryTypes, Meshing
+import Plots
 
 import MultivariatePolynomials, StaticPolynomials
 import StaticArrays: SVector
 const MP = MultivariatePolynomials
 const SP = StaticPolynomials
 
+using Requires: @require
+using DynamicPolynomials: @polyvar
+export @polyvar
+
+function __init__()
+    @require AbstractPlotting = "537997a7-5e4e-5d89-9595-2241ea00577e" include("3d.jl")
+end
 
 """
     compute_z(f, rx, ry)
 Compute all values ``f(x,y)`` for ``(x,y) ∈ rx × ry``.
 """
 compute_z(f::MP.AbstractPolynomialLike, rx, ry) = compute_z(SP.Polynomial(f), rx, ry)
-compute_z(f::SP.Polynomial, rx, ry) = [f(SVector(x,y)) for x in rx, y in ry]
-compute_z(f::Function, rx, ry) = [f(x,y) for x in rx, y in ry]
+compute_z(f::SP.Polynomial, rx, ry) = [f(SVector(x, y)) for x in rx, y in ry]
+compute_z(f::Function, rx, ry) = [f(x, y) for x in rx, y in ry]
 
-
-function implicit_plot(f::MP.AbstractPolynomialLike; scene_resolution=(1000,1000), kwargs...)
-    scene = AbstractPlotting.Scene(resolution=scene_resolution, scale_plot=false)
-    implicit_plot!(scene, f; kwargs...)
+function implicit_plot(f::MP.AbstractPolynomialLike; kwargs...)
+    implicit_plot(f, Val(MP.nvariables(f)); kwargs...)
 end
-function implicit_plot!(scene, f::MP.AbstractPolynomialLike; wireframe=nothing, kwargs...)
+
+function implicit_plot(
+    f::MP.AbstractPolynomialLike,
+    ::Val{2};
+    aspect_ratio = :equal,
+    size = (600, 600),
+    kwargs...,
+)
+    # scene = AbstractPlotting.Scene(resolution = scene_resolution, scale_plot = false)
+    p = Plots.plot(; aspect_ratio = aspect_ratio, size = size)
+    implicit_plot!(p, f; kwargs...)
+end
+
+function implicit_plot!(scene, f::MP.AbstractPolynomialLike; wireframe = nothing, kwargs...)
     if MP.nvariables(f) == 2
         implicit_curve!(scene, f; kwargs...)
     else
-        implicit_surface!(scene, f; wireframe=wireframe, kwargs...)
+        implicit_surface!(scene, f; wireframe = wireframe, kwargs...)
     end
 end
 
 """
-    implicit_curve!(scene, f; x_min=-5, x_max=5, y_min=x_min, y_max=x_max,
+    implicit_curve(f; x_min=-5, x_max=5, y_min=x_min, y_max=x_max,
                       color_curvature=false, color=:steelblue, resolution=1000)
 Visualize the implicit curve `f` in the box `[x_min, x_max] × [y_min, y_max]`.
 If `color_curvature` is `true` then the curve is locally colored depending on its curvature.
 Otherwise `color` is used.
 """
-function implicit_curve!(scene, f::MP.AbstractPolynomialLike; x_min=-3., x_max=3., y_min=x_min, y_max=x_max,
-            color_curvature=false, color=:steelblue, resolution=1000, grid=true, kwargs...)
+function implicit_curve!(
+    p,
+    f::MP.AbstractPolynomialLike;
+    x_min = -3.0,
+    x_max = 3.0,
+    y_min = x_min,
+    y_max = x_max,
+    xlims = (x_min, x_max),
+    ylims = (y_min, y_max),
+    color_curvature = false,
+    color = :steelblue,
+    resolution = 1000,
+    linewidth = 2,
+    grid = true,
+    legend = false,
+    kwargs...,
+)
     g = SP.Polynomial(f)
-    x = collect(range(x_min, x_max; length=resolution))
-    y = collect(range(y_min, y_max; length=resolution))
+    x = collect(range(xlims...; length = resolution))
+    y = collect(range(ylims...; length = resolution))
     z = compute_z(f, x, y)
 
-    limits = AbstractPlotting.FRect(x_min, y_min, x_max - x_min, y_max - y_min)
+
+    # limits = AbstractPlotting.FRect(x_min, y_min, x_max - x_min, y_max - y_min)
     lvl = Contour.contour(x, y, z, 0.0)
     lines = Contour.lines(lvl)
     !isempty(lines) || return scene
     for l in lines
         xs, ys = Contour.coordinates(l)
-        AbstractPlotting.lines!(scene, xs, ys; linewidth=3,
-        color=color, limits=limits, kwargs...)
+
+        Plots.plot!(
+            p,
+            xs,
+            ys;
+            xlims = xlims,
+            ylims = ylims,
+            color = color,
+            legend = legend,
+            linewidth = linewidth,
+            kwargs...,
+        )
     end
 
-    axis = scene[AbstractPlotting.Axis] # get the axis object from the scene
-    if !isnothing(axis)
-        if grid == false
-            axis[:grid][:linewidth] = (0,0)
-        end
-        axis[:ticks][:textsize] = (2, 2)
-        axis[:ticks][:textsize] = (2, 2)
-        axis[:names][:textsize] = (2, 2)
-    end
-    scene
-end
-
-"""
-    implicit_surface!(scene, f; x_min=-3, x_max=3, y_min=x_min, y_max=x_max, z_min=x_min, z_max=z_max,
-                      color_curvature=false, color=:steelblue, resolution=1000)
-Visualize the implicit curve `f` in the box `[x_min, x_max] × [y_min, y_max]`.
-If `color_curvature` is `true` then the curve is locally colored depending on its curvature.
-Otherwise `color` is used.
-"""
-function implicit_surface!(scene, f::MP.AbstractPolynomialLike; x_min=-3., x_max=3., y_min=x_min, y_max=x_max,
-                z_min=x_min, z_max=x_max, color=:steelblue,
-                show_axis=true,
-                wireframe=false,
-                mesh_resolution=0.04, grid=true, kwargs...)
-    g = SP.Polynomial(f)
-    box = GeometryTypes.HyperRectangle(AbstractPlotting.Vec(x_min,y_min, float(z_min)),
-            AbstractPlotting.Vec(float(x_max-x_min),y_max-y_min,z_max-z_min))
-    sdf = Meshing.SignedDistanceField(v -> g(v), box, mesh_resolution)
-    m = Meshing.GLNormalMesh(sdf, Meshing.MarchingTetrahedra())
-    if isnothing(wireframe) || !wireframe
-        AbstractPlotting.mesh!(scene, m, show_axis=show_axis, color=color, kwargs...)
-    else
-        AbstractPlotting.wireframe!(scene, m, show_axis=show_axis, kwargs...)
-    end
-
-    scene
+    p
 end
 
 end # module
