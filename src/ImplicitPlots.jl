173a409e67bc38fe5d96a31992455cb927164daa
Sascha Timme <sascha.timme@gmail.com>
2020-01-28 09:08:36 +0100

    Compat section and cleanup

--- src/ImplicitPlots.jl
+++ src/ImplicitPlots.jl
@@ -1,7 +1,8 @@
 module ImplicitPlots
 
-export implicit_plot
+export implicit_plot, implicit_plot!, implicit_curve!, implicit_surface!
 
+import AbstractPlotting
 import ColorSchemes, Contour
 import GeometryTypes, Meshing
 import Plots
@@ -11,13 +12,8 @@ import StaticArrays: SVector
 const MP = MultivariatePolynomials
 const SP = StaticPolynomials
 
-using Requires: @require
 using DynamicPolynomials: @polyvar
 export @polyvar
-
-function __init__()
-    @require AbstractPlotting = "537997a7-5e4e-5d89-9595-2241ea00577e" include("3d.jl")
-end
 
 make_function(f::MP.AbstractPolynomialLike) = make_function(SP.Polynomial(f))
 function make_function(f::SP.Polynomial)
@@ -53,86 +49,7 @@ implicit_plot(f; kwargs...) = implicit_plot(f, Val(nvariables(f)); kwargs...)
 implicit_plot!(f; kwargs...) = implicit_plot!(nothing, f, Val(nvariables(f)); kwargs...)
 implicit_plot!(p, f; kwargs...) = implicit_plot!(p, f, Val(nvariables(f)); kwargs...)
 
-
-function implicit_plot(
-    f,
-    nargs::Val{2};
-    aspect_ratio = :equal,
-    size = (800, 800),
-    kwargs...,
-)
-    p = Plots.plot(; aspect_ratio = aspect_ratio, size = size)
-    implicit_curve!(p, f; kwargs...)
-end
-implicit_plot!(p, f, nargs::Val{2}; kwargs...) = implicit_curve!(p, f; kwargs...)
-
-"""
-    implicit_curve(f; x_min=-5, x_max=5, y_min=x_min, y_max=x_max,
-                      color_curvature=false, color=:steelblue, resolution=1000)
-Visualize the implicit curve `f` in the box `[x_min, x_max] × [y_min, y_max]`.
-If `color_curvature` is `true` then the curve is locally colored depending on its curvature.
-Otherwise `color` is used.
-"""
-function implicit_curve!(
-    p,
-    f;
-    x_min = -3.0,
-    xmin = x_min,
-    x_max = 3.0,
-    xmax = x_max,
-    y_min = x_min,
-    ymin = y_min,
-    y_max = x_max,
-    ymax = y_max,
-    xlims = (xmin, xmax),
-    ylims = (ymin, ymax),
-    color_curvature = false,
-    color = :steelblue,
-    resolution = 1000,
-    linewidth = 2,
-    grid = true,
-    legend = false,
-    kwargs...,
-)
-    g = make_function(f)
-    rx = range(xlims...; length = resolution)
-    ry = range(ylims...; length = resolution)
-    z = [g(x, y) for x in rx, y in ry]
-
-    lvl = Contour.contour(collect(rx), collect(ry), z, 0.0)
-    lines = Contour.lines(lvl)
-    !isempty(lines) || return p
-
-    for l in lines
-        xs, ys = Contour.coordinates(l)
-
-        if p === nothing
-            Plots.plot!(
-                xs,
-                ys;
-                xlims = xlims,
-                ylims = ylims,
-                color = color,
-                legend = legend,
-                linewidth = linewidth,
-                kwargs...,
-            )
-        else
-            Plots.plot!(
-                p,
-                xs,
-                ys;
-                xlims = xlims,
-                ylims = ylims,
-                color = color,
-                legend = legend,
-                linewidth = linewidth,
-                kwargs...,
-            )
-        end
-    end
-
-    p
-end
+include("2d.jl")
+include("3d.jl")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,21 @@
 using ImplicitPlots
 using Test
 
+import AbstractPlotting
+import Plots
+
 @testset "ImplicitPlots.jl" begin
-    # Write your own tests here.
+
+    f(x,y) = (x^4 + y^4 - 1) * (x^2 + y^2 - 2) + x^5 * y
+    @test implicit_plot(f; xlims=(-2,2), ylims=(-2,2)) isa Plots.Plot
+
+    g(x,y,z) = (0.3*x^2+0.5z-0.3x+1.2*y^2-1.1)^2+(0.7*(y+0.5x)^2+y+1.2*z^2-1)^2-0.3
+    @test implicit_plot(g) isa AbstractPlotting.Scene
+
+    @polyvar x y z
+    f2 = (x^4 + y^4 - 1) * (x^2 + y^2 - 2) + x^5 * y
+    @test implicit_plot(f2; xlims=(-2,2), ylims=(-2,2)) isa Plots.Plot
+
+    g2 = (0.3*x^2+0.5z-0.3x+1.2*y^2-1.1)^2+(0.7*(y+0.5x)^2+y+1.2*z^2-1)^2-0.3
+    @test implicit_plot(g2) isa AbstractPlotting.Scene
 end
